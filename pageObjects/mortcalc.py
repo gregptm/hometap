0f804e65c2cef6ce534a2541625d374dec12e931
greg parsons <gregparsns@gmail.com>
2022-05-30 13:17:30 -0400

    workig POM for all

--- pageObjects/mortcalc.py
+++ pageObjects/mortcalc.py
@@ -4,44 +4,37 @@ from hamcrest import *
 import time
 
 
-class ZillowMortgageCalculator:
+class ZillowMortgageCalculatorPage:
+    '''
+    TODO: need a JIRA Story to set up a proxy
+          and change from verifying monthly P&I to full monthly value
+          l_text_monthlypayment_id
+          see also calling test for changes
+    '''
     URL = "https://www.zillow.com/mortgage-calculator/"
 
-    l_title_interestratehelptooltip_xpath = (By.XPATH,
-                                                                      "(//h4[normalize-space()='Interest rate'])[1]")
-    l_tooltip_x_interestratehelp_xpath = (By.XPATH,
-                                                                   "(//*[name()='svg'][@role='img'])[32]")
-
-    l_label_interestrate_id = (By.ID, 'label_4')
     l_button_interestratehelp_class = (
         By.CLASS_NAME, "TriggerButton-c11n-8-64-1__sc-19o64qd-0 drLVVu")
     l_button_interestratehelp_xpath = (
         By.XPATH, "(//button[@type='button'])[8]")
-
     l_dropdown_loanprogram_id = (By.ID, "form-1_term")
     # note id's are aurogenerated each time so unreliable
     l_errormsg_interestrate_classname = (
         By.CLASS_NAME, "StyledFormHelp-c11n-8-64-1__sc-h3s6hy-0 lfSzwh")
+    l_label_interestrate_id = (By.ID, 'label_4')
     l_link_currentrates_xpath = (
         By.XPATH, "(// a[normalize-space() = 'See current rates'])[1]")
+    l_text_monthlypayment_id = (
+        By.CLASS_NAME, "arc-label-value")  # montly P&I outer arc value
     l_textbox_downpayment_id = (By.ID, "form-1_downPayment")
     l_textbox_downpaymentpercent_id = (
         By.ID, "form-1_downPaymentPercent")
     l_textbox_homeprice_id = (By.ID, "homePrice")
     l_textbox_interestrate_id = (By.ID, "rate")
-
-    l_link_currentrates_xpath = (
-    By.XPATH, "(// a[normalize-space() = 'See current rates'])[1]")
-
-    # StyledButton-c11n-8-64-1__sc-wpcbcc-0iGaQCCzgmi__i17mpm-2 jfcxZT
-    # // a[normalize - space() = 'Full report']
-
-    #l_text_monthlypayment_id = (
-    #    By.XPATH, "//*[name()='text' and contains(@y,'20')]")
-    #l_text_monthlypayment_id = (
-    #    By.XPATH, "//*[ @ "
-    #              'id = "breakdown-panel"]/div/div/div[1]/svg/g/g[4]/text[2]')
-    l_text_monthlypayment_id = (By.CLASS_NAME, "arc-label-value")
+    l_title_interestratehelptooltip_xpath = (
+        By.XPATH, "(//h4[normalize-space()='Interest rate'])[1]")
+    l_tooltip_x_interestratehelp_xpath = (
+        By.XPATH, "(//*[name()='svg'][@role='img'])[32]")
 
     def __init__(self, browser):
         self.browser = browser
@@ -50,94 +43,76 @@ class ZillowMortgageCalculator:
         self.browser.get(self.URL)
 
     def type_entry(self, value, element):
-        print(f"value        : {value}")
-        print(f"element      : {element}")
-        # why why why
+        # why why why !?!
         element.clear()
         element.clear()
         element.click()
-        element.send_keys(value + Keys.TAB )
-        #element.send_keys(Keys.ENTER + Keys.TAB)
-        #element.send_keys(Keys.SHIFT, Keys.TAB)
-        #element.send_keys(Keys.ENTER)
-        theElement = self.browser.find_element(*self.l_text_monthlypayment_id)
-        print(f"the value: {value} the result:  {theElement.text}")
+        element.send_keys(value + Keys.TAB)
+        theResultingMonthly = self.browser.find_element(
+            *self.l_text_monthlypayment_id)
+        print(f"the value: {value} "
+              f"resulting MonthlyP&I: {theResultingMonthly.text}")
 
     def enter_homevalue(self, value):
-         theElement = self.browser.find_element(*self.l_textbox_homeprice_id)
-         print(f"value        : {value}")
-         print(f"theElement   : {theElement}")
-         self.type_entry(value, theElement)
-
-    def enter_downpayment(self, value):
-         theElement = self.browser.find_element(*self.l_textbox_downpayment_id)
-         print(f"value        : {value}")
-         print(f"theElement   : {theElement}")
-         self.type_entry(value, theElement)
-
-    def enter_downpaymentpercent(self, value):
-         theElement = self.browser.find_element(
-             *self.l_textbox_downpaymentpercent_id)
-         print(f"value        : {value}")
-         print(f"theElement   : {theElement}")
-         self.type_entry(value, theElement)
-
-    def enter_interestrate(self, value):
-        theElement = self.browser.find_element(*self.l_textbox_interestrate_id)
-        print(f"value        : {value}")
-        print(f"theElement   : {theElement}")
+        theElement = self.browser.find_element(*self.l_textbox_homeprice_id)
         self.type_entry(value, theElement)
 
+    def enter_downpayment(self, value):
+        theElement = self.browser.find_element(*self.l_textbox_downpayment_id)
+        self.type_entry(value, theElement)
+
+    def enter_downpaymentpercent(self, value):
+        theElement = self.browser.find_element(
+            *self.l_textbox_downpaymentpercent_id)
+        self.type_entry(value, theElement)
+
+    def enter_interestrate(self, value):
+        theElement = self.browser.find_element(
+            *self.l_textbox_interestrate_id)
+        self.type_entry(value, theElement)
 
     def verify_monthlypayment(self, expected_value):
-
-        monthlypayment_pi = self.browser.find_element(*self.l_text_monthlypayment_id)
+        monthlypayment_pi = self.browser.find_element(
+            *self.l_text_monthlypayment_id)
         # Verify Monthly Payment
         print(f"Verify monthly payment : "
-              f"{monthlypayment_pi.text} == {expected_value} "
-              f"{type(monthlypayment_pi.text)} == {type(expected_value)}")
-        assert_that(monthlypayment_pi.text, equal_to(expected_value))
-        print(f"Verify monthly payment : "
               f"{monthlypayment_pi.text} == {expected_value}")
-
-    def click_full_report(self):
-        print("click link full report")
-        self.l_link_fullreport_xpath = (By.XPATH, "//a[normalize-space()='Full report']")
-        fullreport_link = self.browser.find_element(*self.l_link_fullreport_xpath)
-        fullreport_link.click()
-
+        assert_that(monthlypayment_pi.text, equal_to(expected_value))
 
     def test_help_button(self):
         # Click the Help button
         irate_label = self.browser.find_element(*self.l_label_interestrate_id)
-        irate_help_button = self.browser.find_element(*self.l_button_interestratehelp_xpath)
+        irate_help_button = self.browser.find_element(
+            *self.l_button_interestratehelp_xpath)
         irate_help_button.click()
         time.sleep(2)
-        # Verify the tooltip title
-        irate_help_tooltip = self.browser.find_element(*self.l_title_interestratehelptooltip_xpath)
 
+        irate_help_tooltip = self.browser.find_element(
+            *self.l_title_interestratehelptooltip_xpath)
+        # Verify the tooltip title
+        print(f"Verify tooltip title : "
+              f"{irate_help_tooltip.text} == {irate_label.text}")
         assert_that(irate_help_tooltip.text, equal_to(irate_label.text))
-        print(f"Verify tooltip title : {irate_help_tooltip.text} == {irate_label.text}")
 
         # close tooltip
-        irate_help_tooltip = self.browser.find_element(*self.l_tooltip_x_interestratehelp_xpath)
+        irate_help_tooltip = self.browser.find_element(
+            *self.l_tooltip_x_interestratehelp_xpath)
         irate_help_tooltip.click()
 
     def test_current_rates_link(self):
         # Click the Current Rate Link
-        currentrates_link = self.browser.find_element(*self.l_link_currentrates_xpath)
+        currentrates_link = self.browser.find_element(
+            *self.l_link_currentrates_xpath)
         currentrates_link.click()
-        time.sleep(2)
 
-        # Verify Page Title
+        # Switch to tab just opened
         window_name = self.browser.window_handles[1]
-        print(f"window name : {window_name}")
         self.browser.switch_to.window(window_name=window_name)
 
+        # Verify Page Title
         print(f"Verify Page title  : {self.browser.title}")
-        assert_that(self.browser.title, contains_string('Current Mortgage Rates'))
-        time.sleep(2)
+        assert_that(self.browser.title,
+                    contains_string('Current Mortgage Rates'))
 
         # Close the tab opened
         self.browser.close()
-        time.sleep(4)
--- testCases/test_zillowMortCalc.py
+++ testCases/test_zillowMortCalc.py
@@ -1,37 +1,51 @@
 import pytest
-from pageObjects.mortcalc import ZillowMortgageCalculator
-import time
+from pageObjects.mortcalc import ZillowMortgageCalculatorPage
 
 
-def test_test(browser):
+@pytest.mark.L0
+def test_Mortgage_Calculator_Fixed(browser):
+    '''
+    TODO: need a JIRA Story to set up a proxy
+          and change from verifying monthly P&I to full monthly value
+          monthlypayment_amt
+          see also page object for changes
+    '''
     homevalue_amt = "539,000"
     homedownpayment_amt = "107,800"
     downpercentage_amt = "25"
     interestrate_amt = "4.375"
-    monthlypayment_amt = "$2,018"  #"$2,474"
+    monthlypayment_amt = "$2,018"
+    # TODO: "$2,474" Can't  use as there is rotating taxes need constant proxy
 
-    page = ZillowMortgageCalculator(browser)
+    page = ZillowMortgageCalculatorPage(browser)
     page.load()
     print("\n\n")
     print(f"This File's  __name__ is set to: {format(__name__)}")
-    print(f"driver Title : {browser.title}")
-    print(f"Driver name  : {browser.name}")
-    print(f"Driver URL   : {browser.current_url}")
+    print(f"Browser Title : {browser.title}")
+    print(f"Browser name  : {browser.name}")
+    print(f"Browser URL   : {browser.current_url}")
 
+    # oddly had to do out of order, elseL interest changed on us
     print(f"Home Price   : {homevalue_amt}")
-    #page.enter_homevalue(homevalue_amt)
+    page.enter_homevalue(homevalue_amt)
     print(f"Home iRate   : {interestrate_amt}")
-    #page.enter_interestrate(interestrate_amt)
+    page.enter_interestrate(interestrate_amt)
     print(f"Home Down    : {homedownpayment_amt}")
-    #page.enter_downpayment(homedownpayment_amt)
+    page.enter_downpayment(homedownpayment_amt)
     print(f"Home Down %  : {downpercentage_amt}")
-    #page.enter_downpaymentpercent(downpercentage_amt)
+    page.enter_downpaymentpercent(downpercentage_amt)
     # default 30 year value
 
-    #page.verify_monthlypayment(monthlypayment_amt)
+    # Verify monthly P&I number til we get a proxy set up
+    page.verify_monthlypayment(monthlypayment_amt)
 
-    #page.test_help_button()
+
+@pytest.mark.L1
+def test_Interst_Rate_Extras(browser):
+    page = ZillowMortgageCalculatorPage(browser)
+    page.load()
+
+    # Verify Interst Rate Help Icon
+    page.test_help_button()
+    # Verify Current Interest Rates Link
     page.test_current_rates_link()
-
-if __name__ == "__main__":
-    test_test()
